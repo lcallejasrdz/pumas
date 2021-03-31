cc8662522f42c5ba12165bf9cf58257bac966895
Luis Eduardo Callejas Rodríguez <lcallejasrdz@MacBook-Air-de-Luis-3.local>
2021-03-30 19:09:56 -0500

    Images

--- resources/views/website/modules/bases.blade.php
+++ resources/views/website/modules/bases.blade.php
@@ -3,7 +3,7 @@
 		<h2 class="text-center">BASES</h2>
 	</div>
 	<div class="col-md-5 img-module-container">
-		<img src="{{ URL::to('/assets/images/index/flag.png') }}" alt="..." class="padding-bottom-0">
+		<img src="{{ URL::to('/assets/images/index/flag.svg') }}" alt="..." class="padding-bottom-0">
 	</div>
 	<div class="col-md-7">
 		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
